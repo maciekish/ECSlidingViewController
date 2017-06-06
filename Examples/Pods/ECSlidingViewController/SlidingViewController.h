4eb8878a343c46b78b0704b345e4dbe720dd9866
Maciej Swic <maciej@swic.name>
2017-06-06 23:18:32 +0200

    Carthage support

---
+++ Examples/Pods/ECSlidingViewController/SlidingViewController.h
@@ -0,0 +1,18 @@
+//
+//  ECSlidingViewController.h
+//  ECSlidingViewController
+//
+//  Created by Maciej Swic on 2017-06-06.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ECSlidingViewController.
+FOUNDATION_EXPORT double ECSlidingViewControllerVersionNumber;
+
+//! Project version string for ECSlidingViewController.
+FOUNDATION_EXPORT const unsigned char ECSlidingViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECSlidingViewController/PublicHeader.h>
+
+#import <ECSlidingViewController/ECSlidingViewController.h>
